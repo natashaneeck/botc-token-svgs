897b31e41ed1b9689d99aa8efe40f397a3197ef3
Natasha Neeck <natashaneeck@gmail.com>
2026-04-17 19:26:59 -0400

    added sqlite

--- api-calls.py
+++ api-calls.py
@@ -1,14 +1,14 @@
 import requests
 import sqlite3
 import time
-import os
 
 WIKI_API = "https://wiki.bloodontheclocktower.com/api.php"
 HEADERS = {"User-Agent": "BotCTokenMaker"}
 DB_PATH = "botc.db"
 IMAGE_DIR = "images"
 
-def init_db(conn):
+
+def find_chars(db):
     typelist = {"Townsfolk", "Outsiders", "Minions", "Demons"}
     
     
@@ -33,39 +33,89 @@ def init_db(conn):
                 }
                 params |= contDict
             
-            response = requests.get(WIKI_API, params).json()
-            if ("continue" in response["query"]):
+            response = requests.get(WIKI_API, params=params, headers=HEADERS).json()
+            if ("continue" not in response["query"]):
                 shouldContinue = False
             else:
                 cmcontinueVals = response["query"]["continue"]["cmcontinue"]
                 continueVals = response["query"]["continue"]["continue"]
             
+        
+            for page in response["query"]["categorymembers"]:
+                db.execute("""
+                            INSERT OR IGNORE INTO characters  
+                            (page_id, character_type, character_name) 
+                            VALUES (?, ?, ?)
+                            """, (page["pageid"], category, page["title"]))
             
-            #save result in sqlite, probably saving pageid, character type, and title. 
-            # I want to initialize any new entries to "printed" = false & imgfound = false
+            time.sleep(0.5)
             
+            
+def get_images(db):
     # now for each character saved,  I need to iterate on them and request each one's img download url
-    for character in database:
+    
+    total_chars = db.execute("""
+                             SELECT page_id, character_name 
+                             FROM characters
+                             WHERE img_found IS NOT 1
+                             """)
+    
+    for character in total_chars:
+        (id, name) = character
         params = {"action" : "query",
                   "format" : "json",
                   "prop" : "imageinfo",
                   "iiprop" : "url",
-                  "titles" : f"File:Icon_{character.lower()}.png"}
+                  "titles" : f"File:Icon_{name.lower().replace(" ", "").replace("-", "").replace("'", "")}.png"}
         
-        response = requests.get(WIKI_API, params).json()
+        response = requests.get(WIKI_API, params=params, headers=HEADERS).json()
         
         #get imgurl from proper place, save it
         chrpage = next(iter(response["query"]["pages"].values())) #get first val in pages list
+        
+        if chrpage is None or "missing" in chrpage:
+            print("Error accessing image on pageid %s and character name %s", id, name)
+            print(chrpage)
+            continue
+            
+        #print(chrpage) #use for debugging if error catching isn't good enough
         imgurl = chrpage["imageinfo"][0]["url"]
         
-            
-        #if pagekey = -1, log an error because couldnt find the page
+        db.execute("""
+            UPDATE characters 
+            SET img_url = (?), img_found = 1
+            WHERE page_id = (?)
+            """, (imgurl, id)) 
+        print("Found image for ", name)
+        
+        time.sleep(0.5)
+        
     
-    conn.execute("""
-                 
-                 
-                 """)
+    
+def update_db(db):
+    find_chars(db)
+    db.execute("""
+               DELETE FROM characters
+               WHERE character_name = ?
+               """, ("Qutler",))
+    db.commit()
+    get_images(db)
+    db.commit()
+    
 
 def main():
-    conn = sqlite3.connect(DB_PATH)
-    init_db(conn)
+    db = sqlite3.connect(DB_PATH)
+    db.execute("""CREATE TABLE IF NOT EXISTS characters (
+                    page_id         INTEGER PRIMARY KEY,
+                    character_name  TEXT,
+                    character_type  TEXT,
+                    printed         INTEGER DEFAULT 0,
+                    img_found       INTEGER DEFAULT 0,
+                    img_url         TEXT DEFAULT NULL
+                    )
+                """)
+    db.commit()
+    update_db(db)
+    
+if __name__ == "__main__":
+    main()
