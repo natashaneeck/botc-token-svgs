11a06fff1acb9fe811babadd821741e7bc9d6404
Natasha Neeck <natashaneeck@gmail.com>
2026-09-11 16:30:08 -0400

    debugging updates

--- spreadsheet_reader.py
+++ spreadsheet_reader.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import sqlite3
+import openpyxl
 
 
 SHARED_DB_PATH = "botc.db"
 SEPARATE_DB_PATH = "custom.db"
 
 
-
-
 def read_xlsx(path):
-    df = pd.read_excel(path)
+    return pd.read_excel(path)
 
 def read_csv(path):
-    df = pd.read_csv(path)
+    return pd.read_csv(path)
 
-
+def add_to_db(db, df):
+    for row in range(df.shape[0]):
+        name = df.at[row, "Character Name"]
+        type = df.at[row, "Type"]
+        url = df.at[row, "Image URL"]
+        db.execute("""
+                    INSERT OR IGNORE INTO characters  
+                    (character_name, character_type, img_url) 
+                    VALUES (?, ?, ?)
+                    """, (name, type, url))
+        #print(name, type, url)
+    db.commit()
 
 def main():
     db = sqlite3.connect(SEPARATE_DB_PATH) # ask if want separate db, if so, ask for name of script for cleaner storage, assign SEPARATE_DB_PATH to that + .db, and use, otherwise SHARED_DB_PATH
@@ -29,11 +39,11 @@ def main():
                     )
                 """)
     db.commit()
+
+    df = read_xlsx("fall_of_rome.xlsx")
+    add_to_db(db, df)
     
 if __name__ == "__main__":
     main()
-
-
-#todo: make template xlsx and csv files, then make settings specific so that pandas can read them, put them into db properly etcetc, check it works with svgmaker.py
 
 # helping source: https://sscc.wisc.edu/sscc/pubs/dwp/Reading_Data.html
